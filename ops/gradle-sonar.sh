4c0d898353361eb5dd6d4981781ef8fad79752d6
Alkaphreak <electron.libre@gmail.com>
2023-10-02 18:05:36 +0200

    Add gradle-sonar.sh Add marstech-xeno-drive [sonar].run.xml Update marstech-xeno-drive [nativeStart].run.xml

---
+++ ops/gradle-sonar.sh
@@ -0,0 +1,5 @@
+./gradlew sonar \
+-Dsonar.projectKey="${MT_XENO_DRIVE_SONAR_PROJECT_KEY}" \
+-Dsonar.projectName="${MT_XENO_DRIVE_SONAR_PROJECT_NAME}" \
+-Dsonar.host.url="${MT_XENO_DRIVE_SONAR_HOST_URL}" \
+-Dsonar.token="${MT_XENO_DRIVE_SONAR_TOKEN}"
